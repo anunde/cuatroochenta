15ce24e345784e4c569e47d85a9eeb1b1c073bd8
anunde <a.nunez.torre@gmail.com>
2025-01-02 10:40:25 +0100

    Configure Nelmio Api Doc

--- src/Api/Auth/Domain/Exception/UserUnauthorizedException.php
+++ src/Api/Auth/Domain/Exception/UserUnauthorizedException.php
@@ -4,7 +4,7 @@ namespace App\Api\Auth\Domain\Exception;
 
 class UserUnauthorizedException extends \Exception
 {
-    public function __construct(string $message = "", int $code = 0, ?\Exception $previous = null)
+    public function __construct(string $message = "", int $code = 401, ?\Exception $previous = null)
     {
         parent::__construct($message, $code, $previous);
     }
