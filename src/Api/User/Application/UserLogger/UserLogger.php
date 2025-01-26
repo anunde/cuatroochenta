4a6666ea473caaa24302c2e5e6ee3fdcbeb85aa0
anunde <a.nunez.torre@gmail.com>
2025-01-26 10:13:56 +0100

    fix: adding saving clause

--- src/Api/User/Application/UserLogger/UserLogger.php
+++ src/Api/User/Application/UserLogger/UserLogger.php
@@ -10,22 +10,27 @@ use Anunde\Shared\Domain\Exception\NotFoundException;
 
 final class UserLogger
 {
-    public function __construct(
-      private IUserRepository $repository,
-      private IJWTEncoderService $jwtEncoder,
-      private IPasswordEncoder $passwordEnconder
-    ) {}
+  public function __construct(
+    private IUserRepository $repository,
+    private IJWTEncoderService $jwtEncoder,
+    private IPasswordEncoder $passwordEnconder
+  ) {}
 
-    public function __invoke(UserLoggerRequest $command): string
-    {
-      if(null === $user = $this->repository->findUserByEmail($command->getEmail())) {
-        throw new NotFoundException('User not found');
-      }
-
-      if(!$this->passwordEnconder->isValid($command->getPassword(), $user->getPassword()->value())) {
-        throw new UserUnauthorizedException('Invalid credentials');
-      }
-
-      return $this->jwtEncoder->encode(['email' => $user->getEmail()->value(), "exp" => time() + 3600]);
+  public function __invoke(UserLoggerRequest $command): string
+  {
+    if (null === $user = $this->repository->findUserByEmail($command->getEmail())) {
+      throw new NotFoundException('User not found');
     }
+
+    $this->ensurePasswordIsValid($command->getPassword(), $user->getPassword()->value());
+
+    return $this->jwtEncoder->encode(['email' => $user->getEmail()->value(), "exp" => time() + 3600]);
+  }
+
+  private function ensurePasswordIsValid(string $password, string $hash): void
+  {
+    if (!$this->passwordEnconder->isValid($password, $hash)) {
+      throw new UserUnauthorizedException('Invalid credentials');
+    }
+  }
 }
